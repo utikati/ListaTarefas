c6449be3de76034c7e83c4aef7e8dd7b263eb42b
JMartins <jorge.martins2323@gmail.com>
2021-07-28 11:17:54 +0100

    listaTarefas Commit

---
+++ listatarefas/src/App.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import "./App.css";
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: "1",
+      title: "Tarefas a Cumprir",
+      completed: true,
+    },
+  ]);
+
+  const handleTaskAddition = (taskTitle) => {
+    const newTasks = [
+      ...tasks,
+      {
+        title: taskTitle,
+        id: Math.random(10),
+        completed: false,
+      },
+    ];
+
+    setTasks(newTasks);
+  };
+
+  const handleTaskClick = (taskId) => {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) return { ...task, completed: !task.completed };
+
+      return task;
+    });
+
+    setTasks(newTasks);
+  };
+
+  return (
+    <div>
+      <div className="container">
+        <AddTask handleTaskAddition={handleTaskAddition} />
+        <Tasks tasks={tasks} handleTaskClick={handleTaskClick} />
+      </div>
+    </div>
+  );
+};
+
+export default App;
